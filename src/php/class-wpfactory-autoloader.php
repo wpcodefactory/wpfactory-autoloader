9725b9e138194fb9b291e5403ee7d597e2e717e0
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2024-09-12 23:24:04 -0300

    Standards

--- src/php/class-wpfactory-autoloader.php
+++ src/php/class-wpfactory-autoloader.php
@@ -70,18 +70,18 @@ if ( ! class_exists( 'WPFactory\WPFactory_Autoloader' ) ) {
 		 */
 		public function add_namespace( $prefix, $base_dir, $prepend = false ) {
 
-			// normalize namespace prefix.
+			// Normalize namespace prefix.
 			$prefix = trim( $prefix, '\\' );
 
-			// normalize the base directory with a trailing separator.
+			// Normalize the base directory with a trailing separator.
 			$base_dir = rtrim( $base_dir, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR;
 
-			// initialize the namespace prefix array.
+			// Initialize the namespace prefix array.
 			if ( isset( $this->prefixes[ $prefix ] ) === false ) {
 				$this->prefixes[ $prefix ] = array();
 			}
 
-			// retain the base directory for the namespace prefix.
+			// Retain the base directory for the namespace prefix.
 			if ( $prepend ) {
 				array_unshift( $this->prefixes[ $prefix ], $base_dir );
 			} else {
@@ -132,7 +132,7 @@ if ( ! class_exists( 'WPFactory\WPFactory_Autoloader' ) ) {
 				return true;
 			}
 
-			// never found a mapped file.
+			// Never found a mapped file.
 			return false;
 		}
 
@@ -166,7 +166,7 @@ if ( ! class_exists( 'WPFactory\WPFactory_Autoloader' ) ) {
 				}
 			}
 
-			// never found it.
+			// Never found it.
 			return false;
 		}
 
